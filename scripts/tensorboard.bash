b631e25b9324442c4b3bbbcb41cc173aef9035f8
lloyd-1000095 <lloyd@kostas-ap.localdomain>
2020-08-22 06:03:23 -0500

    syn files

---
+++ scripts/tensorboard.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --output=/scratch/srinath/Code/unsup_keyp_torch/job_logs/tb/job-tensorboard-%j.out
+#SBATCH --qos=viz
+#SBATCH --partition=viz
+#SBATCH --cores=1
+
+PORT_MAP=/tmp/tensorboard_port_map
+
+TB_PORT=$(cat $PORT_MAP | grep "$SLURM_JOBID," | cut -d',' -f2)
+IP_ADDRESS=$(hostname -I | cut -d' ' -f1)
+
+TB_FOLDER=`echo $@ |  sed s/" "/,/g | cut -d "," -f1-`
+
+echo "Go to http://$IP_ADDRESS:$TB_PORT"
+echo $TB_FOLDER
+
+source /scratch/srinath/venvs/tf1-dev/bin/activate
+tensorboard --logdir $TB_FOLDER --port $TB_PORT
+
---
+++ test.py
@@ -0,0 +1,1 @@
+print('addition', 2+4)
