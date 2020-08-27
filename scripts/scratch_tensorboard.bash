7e1f69914e3a8f7d02466344c991006fe8af73c7
lloyd-1000095 <lloyd@kostas-ap.localdomain>
2020-08-27 07:07:45 -0500

    clean and organize files

--- scripts/scratch_tensorboard.bash
+++ scripts/scratch_tensorboard.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --output=/job_logs/tb/job-tensorboard-%j.out
+#SBATCH --output=/scratch/lloyd/git-repo/unsup_keyp_torch/job_logs/tb/job-tensorboard-%j.out
 #SBATCH --qos=viz
 #SBATCH --partition=viz
 #SBATCH --cores=1
@@ -14,7 +14,6 @@ TB_FOLDER=`echo $@ |  sed s/" "/,/g | cut -d "," -f1-`
 echo "Go to http://$IP_ADDRESS:$TB_PORT"
 echo $TB_FOLDER
 
-#source activate myenv
-#conda activate myenv
-source /scratch/srinath/venvs/tf1-dev/bin/activate
+source /scratch/lloyd/venvs/tf1-dev/bin/activate
 tensorboard --logdir $TB_FOLDER --port $TB_PORT
+
